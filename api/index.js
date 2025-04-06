bd891b795d216d55b6266f67a9246f014ca9ee49
kaleaziz <aziz.kale@emm-it.de>
2025-04-07 01:29:39 +0200

    .

--- api/index.js
+++ api/index.js
@@ -39,37 +39,6 @@ app.get("/send-email", async (req, res) => {
   res.send("Email has been sent!");
 });
 
-// Mail gönderme işlevini yazalım
-const sendEmail = async () => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "muruvvet.goettingen@gmail.com", // Kendi e-posta adresinizi girin
-      pass: "qscu dyxn eolg khfg", // Gmail App Password
-    },
-  });
-
-  const mailOptions = {
-    from: "your-email@gmail.com", // Gönderen e-posta
-    to: "recipient-email@example.com", // Alıcı e-posta
-    subject: "Test Email from Node.js",
-    text: "This is a test email sent using Nodemailer and Gmail App Password in Node.js!",
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully!");
-  } catch (error) {
-    console.log("Error sending email:", error);
-  }
-};
-
-// Express route to trigger email sending
-app.get("/send-email", async (req, res) => {
-  await sendEmail();
-  res.send("Email has been sent!");
-});
-
 module.exports = (req, res) => {
   app(req, res); // Vercel'in beklediği şekilde export
 };
